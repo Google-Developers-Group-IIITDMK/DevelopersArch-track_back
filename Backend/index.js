23446431aaad20beb559c7210fee487f2382153d
Myself-Praveen <praveen104685@gmail.com>
2025-09-21 23:28:48 +0530

    Add user authentication

--- Backend/index.js
+++ Backend/index.js
@@ -24,3 +24,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
+
+import authRoutes from "./routes/authRoutes.js";
+
+app.use(express.json()); 
+app.use("/api/auth", authRoutes);
