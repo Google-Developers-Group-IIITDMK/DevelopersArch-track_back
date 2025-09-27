b5fcd8ed866e87563c1e12f6d5c26f008ee4539f
Samayra312007 <samayra312007@gmail.com>
2025-09-27 15:52:02 +0530

    update location in protectedRoute

--- Frontend/src/pages/ProtectedRoute.jsx
+++ Frontend/src/pages/ProtectedRoute.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 
 const ProtectedRoute = ({ children }) => {
   const { currentUser, loading } = useAuth();
+  const location = useLocation();
 
   if (loading) {
     return (
