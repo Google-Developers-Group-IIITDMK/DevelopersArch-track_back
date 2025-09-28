fb2471a2ed81758a1e5ac83b7d8e45593f2a66f7
Samayra312007 <samayra312007@gmail.com>
2025-09-28 22:37:27 +0530

    change the cors origin to deployed cors origin

--- Backend/index.js
+++ Backend/index.js
@@ -12,10 +12,12 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://trackback-three.vercel.app",
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("hello world");
